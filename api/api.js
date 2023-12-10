a2bddfd78aced58423c0fe0072469a5fbc5d2498
kangdayeong <kangdy7599@gmail.com>
2023-12-10 23:03:23 +0900

    Feat : api 및 chat 공부

--- api/api.js
+++ api/api.js
@@ -1,5 +1,49 @@
-const $app = document.getElementById("app");
-const $test = document.createElement("a");
-$test.innerText = "api 테스트";
-$app.append($test);
+const $postBtn = document.getElementById("postForm");
+const $getBtn = document.getElementById("getRequest");
 
+
+
+
+
+
+
+
+// POST 요청 처리
+function func(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var score = parseInt(document.getElementById("score").value);
+
+    fetch('http://113.198.233.57:3000/api/test/record', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name, score: score }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("result").innerHTML += 'POST Response: ' + JSON.stringify(data) + '<br>';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+$postBtn.addEventListener("submit", func);
+
+
+
+
+function fun() {
+    fetch('http://113.198.233.57:3000/api/test/record?type=score')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("result").innerHTML += 'GET Response: ' + JSON.stringify(data) + '<br>';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+// GET 요청 처리
+$getBtn.addEventListener("click", fun);
--- chat/chat.js
+++ chat/chat.js
@@ -16,37 +16,65 @@ $app.appendChild($sendMessage);
 $app.appendChild($messages);
 
 
-
-
 // 오늘 날짜 표시
 const today = new Date();
-$today.innerText = `오늘의 날짜: ${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
+$today.innerText = `오늘의 날짜: ${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
 // 유저 수 표시
 socket.on('joinUser', (data) => {
   $userCount.innerText = `유저 수: ${data.userCount}`;
 });
 
+// const data ={
+//  userName : '다영',
+//  message : '123',
+//  timeStamp : new Date()
+// }
+
 const userName = '다영'; // 사용자 이름 설정
 // 메시지 수신
 socket.on('receiveMessage', (data) => {
-    const $message = document.createElement('div');
-    $message.innerText = `${data.userName}: ${data.message} (보낸 시간: ${new Date(data.timeStamp).toLocaleTimeString()})`;
+  const $message = document.createElement('div');
+  $message.innerText = `${data.userName}: ${data.message} (보낸 시간: ${new Date(data.timeStamp).toLocaleTimeString()})`;
+
+  // 사용자가 보낸 메시지인 경우 배경색 변경
+  if (data.userName === userName) {
+    $message.style.backgroundColor = '#a2d5f2'; // 예시로 파란색 계열 배경 적용
+    $message.style.color = 'white'; // 텍스트 색상 변경
+    $message.style.padding = '5px'; // 패딩 추가
+    $message.style.borderRadius = '5px'; // 경계 둥글게
+  }
+
+  $messages.appendChild($message);
+});
+
+
+function fun (){
+  const message = $messageInput.value;
+// debugger
+  const userName = '다영'; // 사용자 이름 설정
+  socket.emit('sendMessage', { userName, message });
+  $messageInput.value = '';
   
+
+  const $message = document.createElement('div');
+  $message.innerText = `${userName} : ${message} 보낸시간 : ${new Date(new Date().getTime()).toLocaleTimeString()}`;
     // 사용자가 보낸 메시지인 경우 배경색 변경
-    if (data.userName === userName) {
+    if (userName ) {
       $message.style.backgroundColor = '#a2d5f2'; // 예시로 파란색 계열 배경 적용
       $message.style.color = 'white'; // 텍스트 색상 변경
       $message.style.padding = '5px'; // 패딩 추가
       $message.style.borderRadius = '5px'; // 경계 둥글게
     }
-  
-    $messages.appendChild($message);
-  });
+  $messages.appendChild($message);
+}
 
 // 메시지 전송
-$sendMessage.addEventListener('click', () => {
-  const message = $messageInput.value;
-  socket.emit('sendMessage', { userName: '다영', message });
-  $messageInput.value = '';
-});
+$sendMessage.addEventListener('click', fun);
+
+
+// $sendMessage.addEventListener('click', () => {
+//   const message = $messageInput.value;
+//   socket.emit('sendMessage', { userName: '다영', message });
+//   $messageInput.value = '';
+// });
